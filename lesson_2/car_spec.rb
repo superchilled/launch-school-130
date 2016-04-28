c3ba9e70868f2575c9cca703f7da3eda64f797ef
superchilled <karl@superchilled.co.uk>
2016-04-28 17:21:56 +0000

    Adding lesson 2 files

---
+++ lesson_2/car_spec.rb
@@ -0,0 +1,14 @@
+# car_spec.rb
+
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'car'
+
+describe 'Car#wheels' do
+  it 'has 4 wheels' do
+    car = Car.new
+    car.wheels.must_equal 4           # this is the expectation
+  end
+end
